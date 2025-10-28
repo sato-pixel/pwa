3be340dfdb0e0607b37048fb6c5dbf827c4f29b4
sato-pixel <sato@nagoyagaiko.net>
2025-10-28 09:43:51 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-sample-v2-sato-pixel-pwa';
+const CACHE_NAME = 'pwa-sample-v1';
 const FILES_TO_CACHE = [
 './',
 './index.html',
@@ -11,15 +11,6 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', (event) => {
 event.waitUntil(
 caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-);
-});
-
-
-self.addEventListener('activate', (event) => {
-event.waitUntil(
-caches.keys().then(keys => Promise.all(
-keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-))
 );
 });
 
